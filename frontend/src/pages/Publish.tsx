914fba7d931ba8a647403c0a10d071d4dc99ecda
Shashidhar <shashidharb459@gmail.com>
2024-09-08 12:50:48 +0530

    updated publish page

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,12 +1,14 @@
 import { BlogEditor } from "../components/BlogEditor";
 import { Appbar } from "../components/Appbar";
+import { BlogFooterBar } from "../components/BlogFooter";
 
 export const Publish = () => {
     return (
         <div className="publish-container">
             <Appbar></Appbar>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 m-5">
+            <div className="grid grid-cols-1 gap-8 m-5 justify-center items-center max-w-5xl mx-auto">
                 <BlogEditor />
+                <BlogFooterBar></BlogFooterBar>
             </div>
         </div>
     );
