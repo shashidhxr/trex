e9e79b61924f310f9c77c7100581ad1f03bb28d5
Shashidhar <shashidharb459@gmail.com>
2025-01-20 16:09:14 +0530

    fixed blogcard alignment

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -16,21 +16,23 @@ export const BlogCard = ({
     publishedDate,
 }: CardProps) => {
     return (
-        <Link to={`/blog/${id}`}>
-            <div className="w-full bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-                <div className="pr-[440px] p-6 m-5 flex flex-col space-y-4">
-                    <div className="text-sm text-gray-500">
-                        <span className="font-semibold">{authorName}</span> &middot; {publishedDate}
-                    </div>
-                    <div className="text-xl font-bold text-gray-800">
-                        {title}
-                    </div>
-                    <div className="text-gray-700 line-clamp-2">
-                        {content}
+        <Link to={`/blog/${id}`} className="block no-underline">
+            <div className="w-full h-44 p-2 my-3 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+                <div className="h-full p-6 flex flex-col">
+                    <div className="space-y-3">
+                        <div className="text-sm text-gray-500 ">
+                            <span className="font-semibold">{authorName}</span> &middot; {publishedDate}
+                        </div>
+                        <div className="text-xl font-bold text-gray-800">
+                            {title}
+                        </div>
+                        <div className="text-gray-700 line-clamp-2">
+                            {content}
+                        </div>
                     </div>
                 </div>
             </div>
         </Link>
-
-    );
+    )
 };
+
